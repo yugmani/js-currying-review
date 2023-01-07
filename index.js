c3f94ea11c42673ee8739ae56f41e45b30cbd5d1
yugmani <yugmani@yahoo.com>
2023-01-07 00:38:16 -0800

    this keyword

--- index.js
+++ index.js
@@ -30,3 +30,54 @@ const add3 = add2(3);
 const add5 = add3(5);
 
 console.log(add5); //10
+
+// Review of 'this' keyword
+// *********************************
+
+function sayMyName(name) {
+  console.log(`My name is ${name}`);
+}
+
+sayMyName('Walter White');
+sayMyName('Heisenburg');
+
+//determing 'this'
+// ----------------
+
+//a. Implicit binding
+const person = {
+  name: 'Mary',
+  sayMyName: function () {
+    console.log(`My name is ${this.name}`);
+  },
+};
+
+person.sayMyName();
+
+//b. Explicit binding
+
+function saySomething() {
+  console.log(`My name is ${this.name}`);
+}
+
+saySomething.call(person);
+
+//c. New binding
+
+function Person(name) {
+  //this = {}
+  this.name = name;
+}
+
+const p1 = new Person('Viswas');
+const p2 = new Person('Batman');
+console.log(p1.name);
+console.log(p2.name);
+
+//d. Default binding
+
+//test this section in Vistual Studio Code editor.
+// const name = 'Superman';
+// globalThis.name = 'Superman';
+
+// saySomething();
